f3e799cfa8ed920d412d0b0b0e97788ce47f7945
Colin Rubbert <colinrubbert@gmail.com>
2015-05-08 01:16:32 +0000

    Create about page and routes, add content and style about page, add images to assets

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Splurty::Application.routes.draw do
 
   # Routes
   root 'quotes#index'
-
   resources :quotes
+  get 'about', :to => 'quotes#about'
   # The priority is based upon order of creation: first created -> highest priority.
-  # More comments here .....
+
 
 end
