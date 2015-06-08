3a84e9ef874e8effb60f2a90116511e6c4f5a846
Colin Rubbert <colinrubbert@gmail.com>
2015-06-08 14:47:50 +0000

    Create a passiong test for building a unique quote tag, implement unique_tag to application

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
+
+  test "unique_tag" do
+    quote = Quote.create(:author => 'Marcus Aurelius', :saying => 'The impediment to action advances action. What stands in the way becomes the way.')
+    expected = 'MA#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
+  end
+
 end
